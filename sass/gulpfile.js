81d4762df1499b4e233b5b643a349906abf65980
ricirossi <matheus.ricieri@hotmail.com>
2023-08-23 13:28:25 -0300

    Add partials and @imports content
    #4 Partials and @imports

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 
 // compilar arquivo scss em css
 function buildStyles() {
-  return src("index.scss") // caminho relativo do arquivo scss passado para src
+  return src("*.scss") // caminho relativo do arquivo scss passado para src
     .pipe(sass())
     .pipe(dest("css"));
 }
 
 // vigiar mudanças no arquivo scss e chamar buildStyle
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
