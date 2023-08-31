c58771631642eea9162e944f09633957b975366f
ricirossi <matheus.ricieri@hotmail.com>
2023-08-31 20:08:08 -0300

    Add nuxt content
    Started learning nuxt

---
+++ nuxt/dojo/pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia vitae
+      recusandae nihil eaque sunt distinctio esse hic fugit vel dolore?
+    </p>
+  </div>
+</template>
+
+<script setup>
+// o valor dentro de { } deve ser o mesmo do arquivo.s
+const { id } = useRoute().params;
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
