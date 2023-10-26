a4df14837930621766f8bcb350e16d2c484a777b
Ricieri <matheus.ricieri@hotmail.com>
2023-10-26 10:11:59 -0300

    Finish express study

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,12 +1,45 @@
 const express = require("express");
 const app = express();
-const saudacao = require("./saudacaoMid");
+const bodyParser = require("body-parser");
 
-app.use(saudacao("Guilherme"));
+const saudacao = require("./saudacaoMid");
+const usuarioApi = require("./api/usuario");
+require("./api/produto")(app, "com param");
+
+app.post("/usuario", usuarioApi.salvar);
+app.get("/usuario", usuarioApi.obter);
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(saudacao("Ricieri"));
 
 app.use("/opa", (req, res, next) => {
     console.log("Antes");
     next();
+});
+
+app.get("/clientes/relatorio", (req, res) => {
+    res.send(
+        `Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`
+    );
+});
+
+app.post("/corpo", (req, res) => {
+    // let corpo = "";
+    // req.on("data", function (parte) {
+    //     corpo += parte;
+    // });
+
+    // req.on("end", function () {
+    //     res.send(corpo);
+    // });
+    res.send(req.body); // usado com o bodyParser
+});
+
+app.get("/clientes/:id", (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`);
 });
 
 app.get("/opa", (req, res, next) => {
@@ -29,6 +62,6 @@ app.use("/opa", (req, res) => {
     console.log("Depois");
 });
 
-app.listen(3001, () => {
-    console.log("Backend executando na porta 3001");
+app.listen(3000, () => {
+    console.log("Backend executando na porta 3000");
 });
